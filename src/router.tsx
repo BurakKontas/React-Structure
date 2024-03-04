4a830320551802e91a013b24518267ffa8c6418d
BurakKontas <aburakkontas@trakya.edu.tr>
2024-03-04 20:48:10 +0300

    Update router to use BrowserRouter instead of HashRouter

--- src/router.tsx
+++ src/router.tsx
@@ -1,14 +1,14 @@
-import { Route, Routes, HashRouter } from "react-router-dom";
+import { Route, Routes, BrowserRouter } from "react-router-dom";
 import { Homepage } from "@Pages/homepage";
 // import Homepage from "@Pages/homepage/Homepage";
 
 const Router = () => {
     return (
-        <HashRouter>
+        <BrowserRouter>
             <Routes>
                 <Route path="/" element={<Homepage />} />
             </Routes>
-        </HashRouter>
+        </BrowserRouter>
     );
 };
 
